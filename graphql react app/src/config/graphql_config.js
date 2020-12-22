c2a58c629832ca17e923de81b0f95ea8da999840
Asad Ali <asadalibest1@gmail.com>
2020-12-22 15:48:12 +0500

    Delete graphql_config.js

--- graphql react app/src/config/graphql_config.js
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-// import { ApolloClient, InMemoryCache } from 'apollo-boost';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache()
-});
-
-export default client;
